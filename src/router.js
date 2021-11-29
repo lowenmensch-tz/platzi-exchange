f3cf019d1e4fd68e52a58dff21d75aaf0d7ac025
lowenmensch-tz <kenneth-cruz@outlook.com>
2021-11-28 18:27:01 -0600

    Curso Básico de Vue.js 2, proyecto 'Platzi exchange'

---
+++ src/router.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Home from '@/views/Home';
+import About from '@/views/About';
+import Error from '@/views/Error';
+import CoinDetail from '@/views/CoinDetail';
+
+Vue.use(Router);
+
+// Rutas
+export default new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        }, 
+        {
+            path: '/coin/:id',
+            name: 'coin-detail',
+            component: CoinDetail,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About 
+        }, 
+        {
+            path: '*', 
+            name: 'Error', 
+            component: Error
+        }
+    ]
+});
